50b1ef65d713844443e435649ce8b7981d2ddad8
Kelvin Chao <kelvinchao2011@gmail.com>
2022-03-11 11:21:28 -0800

    cleaned up lat long fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@
 
 Tasks:
 -[X] get input from user
--[_] set search to local storage
+-[X] set search to local storage
 -[_] create getLocalStorage for preload
 -[_] list prior searches
 -[_] api call for lat/long position
@@ -21,64 +21,59 @@ Optional:
 //                          VARIABLES                              //
 /////////////////////////////////////////////////////////////////////
 
-var cityName = $('#city-name')
-var submit_button = $('#searcher')
+var cityName = $("#city-name");
+var submit_button = $("#searcher");
 
+var prior_search_div = $("#prior-search");
 
+var today_div = $("#weather-today");
+var five_day_div = $("#five-day");
 
-var prior_search_div = $('#prior-search')
+var excl = "minutely,hourly,alerts";
 
-var today_div = $('#weather-today')
-var five_day_div = $('#five-day')
-
-var excl = 'minutely,hourly,alerts'
-
-var cityName, lat, lon, url_geo_reqeust, url_request, cityInfo
+var cityName, lat, lon, url_geo_reqeust, url_request, cityInfo;
 
 /////////////////////////////////////////////////////////////////////
 //                          FUNCTIONS                              //
 /////////////////////////////////////////////////////////////////////
 
-
 function text_to_api() {
-  cityName = $('#city-name').val().toUpperCase()
-
+  cityName = $("#city-name").val().toUpperCase();
+  cityInfo = {
+    'cityName': cityName,
+    'lat': 0,
+    'lon': 0,
+    'geo_reqeust_url': "",
+  };
 
   // get lat long
-  // http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key} 
-  var geo_reqeust_url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=b5160f6261b60ed2e93a3754a8a382bc`
+  // http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}
+  cityInfo.geo_reqeust_url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=b5160f6261b60ed2e93a3754a8a382bc`;
 
-  fetch(geo_reqeust_url) 
-    .then(function(response) {
-      var lat = response.lat
-      var lon = response.lon
-      console.log(lat, lon, response)
-      return [response.json(), lat, lon];
+  fetch(cityInfo.geo_reqeust_url)
+    .then((response) => {
+      return response.json();
     })
-    
-  
+
+    .then((data) => {
+      cityInfo.lat = data[0].lat;
+      cityInfo.lon = data[0].lon;
+      console.log(cityInfo);
+      return [cityInfo];
+    });
 
   // api call for weather data
   // var url_request = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${excl}&appid=${secretKey}`
   // var url_request = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${excl}&appid=b5160f6261b60ed2e93a3754a8a382bc`
- 
-  // Saving search to local storage
-  
-  var stored_city = JSON.parse(localStorage.getItem('city')) || [];
-  stored_city.push({'cityName':cityName})
-  
-  localStorage.setItem('city', JSON.stringify(stored_city))
 
 
-  cityInfo = {'cityName':cityName, 
-              'lat':lat, 
-              'lon':lon, 
-              'geo_reqeust_url':geo_reqeust_url}
+  var stored_city = JSON.parse(localStorage.getItem("city")) || [];
+  stored_city.push(cityInfo);
 
-  return cityInfo
+  localStorage.setItem("city", JSON.stringify(stored_city));
+
+  return cityInfo;
 }
-
-
 
 // fetch requests
 /* 
@@ -90,22 +85,22 @@ fetch(text_to_api().url_geo_request) (
 
 */
 
-function loadPriorSearch() {
-
+function saveCityInfo(cityInfo) {
+  var stored_city = JSON.parse(localStorage.getItem("city")) || [];
+  localStorage.setItem("city", JSON.stringify(stored_city));
 }
 
-
+function loadPriorSearch() {}
 
 /////////////////////////////////////////////////////////////////////
 //                             MAIN                                //
 /////////////////////////////////////////////////////////////////////
 
 function main() {
-  submit_button.on('click', () => {
-    console.log("clicked")
-    text_to_api()
-    console.log($('#city-name').val())
-  })
-
+  submit_button.on("click", () => {
+    console.log("clicked");
+    text_to_api();
+    console.log($("#city-name").val());
+  });
 }
-main()
+main();
